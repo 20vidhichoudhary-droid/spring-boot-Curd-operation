fae8b8aa4169b1a0d8146a4332d47acab2293863
20vidhichoudhary-droid <20vidhichoudhary@gmail.com>
2025-11-15 11:55:27 +0530

    Add files via upload

---
+++ SBcurdoperationClass.java
@@ -0,0 +1,13 @@
+package SB_curd_operation_Pacakge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBcurdoperationClass
+{
+public static void main(String[] args) 
+{
+    SpringApplication.run(SB_curd_operation_Pacakge.SBcurdoperationClass.class, args);
+}
+}
